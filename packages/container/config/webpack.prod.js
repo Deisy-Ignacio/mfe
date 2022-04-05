980749e4e51499f44dad1fed8ec7e1dfc352fa2b
IgnacioLuisLuzDeisy <nissy_14@hotmail.com>
2022-04-05 13:08:17 -0500

    added public path

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -8,6 +8,7 @@ const prodConfig = {
   mode: "production",
   output: {
     filename: "[name].[contenthast].js",
+    publicPath: "/container/latest/",
   },
   plugins: [
     new ModuleFederationPlugin({
